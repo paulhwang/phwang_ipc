dce307da35bbd530f9d7b34bebda7a24a16814d6
paul.hwang <paul.hwang@getac.com.tw>
2018-10-16 09:36:05 +0800

    path

--- SWRDC_IPC/code/SwdcIpc/SwrdcIpcPath.cs
+++ SWRDC_IPC/code/SwdcIpc/SwrdcIpcPath.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,21 @@ namespace GetacSwrdc.IpcPath
 {
     class IpcPathClass
     {
+        private IpcTcp.IpcCoreClass ipc_core = new IpcTcp.IpcCoreClass();
+
+        void TransmitData (int path_id_var, string data_var)
+        {
+            IpcPathEntryClass path_entry = GetPath(path_id_var);
+            ipc_core.TcpTransmitData(path_entry.TcpStream, data_var);
+        }
+
+        string ReceiveData (int path_id_var)
+        {
+            IpcPathEntryClass path_entry = GetPath(path_id_var);
+            string data = ipc_core.TCpReceiveData(path_entry.TcpStream);
+            return null;
+        }
+
         IpcPathEntryClass GetPath (int path_id_var)
         {
             return null;
@@ -26,7 +42,7 @@ namespace GetacSwrdc.IpcPath
 
     class IpcPathEntryClass
     {
-
+        public NetworkStream TcpStream;
     }
 
 }
