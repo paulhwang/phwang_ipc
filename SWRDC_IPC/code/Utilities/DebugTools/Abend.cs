d9d23010f2c7f1705d7f34171a340ef51abc3ebf
paul.hwang <paul.hwang@getac.com.tw>
2018-10-17 11:57:33 +0800

    abend

---
+++ SWRDC_IPC/code/Utilities/DebugTools/Abend.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getac.Csc.Utilities
+{
+    class DebugClass
+    {
+        public static void DebugIt(string var1, string var2)
+        {
+            DebugIt_(var1, var2);
+        }
+
+        static void DebugIt_(string var1, string var2)
+        {
+            System.Diagnostics.Debug.WriteLine(var2, var1);
+        }
+
+        public static void AbendIt(string var1, string var2)
+        {
+            System.Diagnostics.Debug.WriteLine("***", "ABEND");
+            System.Diagnostics.Debug.WriteLine(var2, var1);
+            System.Diagnostics.Debug.WriteLine("***", "ABEND");
+        }
+    }
+}
